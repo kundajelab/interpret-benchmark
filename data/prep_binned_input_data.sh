9623fb9d94dd242d0798d224a69987967f2f75c7
AvantiShri <avanti.shrikumar@gmail.com>
2018-11-01 01:13:07 -0700

    updated data prep scripts

--- data/prep_binned_input_data.sh
+++ data/prep_binned_input_data.sh
@@ -4,6 +4,7 @@ INP_FILE_PATHS="data_paths.txt"
 ALL_NAIVE_PEAKS="all_naive_peaks.bed.gz"
 MERGED_NAIVE_PEAKS="merged_naive_peaks.bed.gz"
 ALL_BINS="all_bins.bed.gz"
+LABELS_OUT="all_labels.bed"
 
 [[ -e $ALL_NAIVE_PEAKS ]] && rm $ALL_NAIVE_PEAKS
 [[ -e $MERGED_NAIVE_PEAKS ]] && rm $MERGED_NAIVE_PEAKS
@@ -13,7 +14,7 @@ ALL_BINS="all_bins.bed.gz"
 if [ -e $ALL_NAIVE_PEAKS ]; then rm $ALL_NAIVE_PEAKS; fi
 while IFS='' read -r line || [[ -n "$line" ]]; do
     task_name=$(echo $line | cut -d" " -f1)
-    idr_path=$(echo $line | cut -d" " -f2)
+    #idr_path=$(echo $line | cut -d" " -f2)
     naive_path=$(echo $line | cut -d" " -f3)
     zcat $naive_path | gzip -c >> $ALL_NAIVE_PEAKS
 done < $INP_FILE_PATHS
@@ -31,15 +32,24 @@ zcat $ALL_BINS | wc -l
 #do a bedtools intersect with all the naive peaks and idr peaks
 while IFS='' read -r line || [[ -n "$line" ]]; do
     task_name=$(echo $line | cut -d" " -f1)
-    idr_path=$(echo $line | cut -d" " -f2)
+    #idr_path=$(echo $line | cut -d" " -f2)
     naive_path=$(echo $line | cut -d" " -f3)
 
     [[ -d $task_name ]] || mkdir $task_name
     echo $task_name
-    intersectBed -e -f 0.5 -F 0.5 -a $ALL_BINS -b $idr_path -wa | gzip -c > $task_name/idr_optimal_overlaps.bed.gz
-    echo "idr optimal regions"
-    zcat $task_name/idr_optimal_overlaps.bed.gz | wc -l
+    #intersectBed -e -f 0.5 -F 0.5 -a $ALL_BINS -b $idr_path -wa | gzip -c > $task_name/idr_optimal_overlaps.bed.gz
+    #echo "idr optimal regions"
+    #zcat $task_name/idr_optimal_overlaps.bed.gz | wc -l
     intersectBed -e -f 0.5 -F 0.5 -a $ALL_BINS -b $naive_path -wa | gzip -c > $task_name/naive_overlaps.bed.gz
     echo "naive regions"
     zcat $task_name/naive_overlaps.bed.gz | wc -l
 done < $INP_FILE_PATHS
+
+echo "preparing labels file"
+
+./label_bins.py $LABELS_OUT
+gzip $LABELS_OUT
+
+zcat $LABELS_OUT".gz" | egrep -w 'chr1|chr8|chr21' | gzip -c > "test_"$LABELS_OUT".gz"
+zcat $LABELS_OUT".gz" | egrep -w 'chr9' | gzip -c > "valid_"$LABELS_OUT".gz"
+zcat $LABELS_OUT".gz" | egrep -w -v 'chr1|chr8|chr21|chr9' | gzip -c > "train_"$LABELS_OUT".gz"
