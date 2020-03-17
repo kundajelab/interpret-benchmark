4b9165e1b4631dc37a3db641ed3971e0d95ca68e
AvantiShri <avanti.shrikumar@gmail.com>
2020-03-16 21:57:29 -0700

    generating simulation

--- setup.py
+++ setup.py
@@ -10,5 +10,6 @@ if __name__== '__main__':
           setup_requires=[],
           install_requires=['numpy>=1.9'],
           scripts=['scripts/generate_pos_and_neg_fasta',
-                   'scripts/homer2meme'],
+                   'scripts/homer2meme',
+                   'scripts/generate_simulated_pos_and_neg'],
           name='vakai')
