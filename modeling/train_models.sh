9bae53e133e16c85f2e4e940891d9504294584ff
Eva Prakash <evaprakash2@gmail.com>
2021-10-06 22:40:41 -0700

    Added modeling scripts

---
+++ modeling/train_models.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+momma_dragonn_train --hyperparameter_configs_list config/hyperparameter_configs_list.yaml --end_of_training_callbacks_config config/end_of_training_callbacks_config.yaml
